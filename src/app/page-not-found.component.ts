02920dd564b6091c0342d54c4dd57a657339252c
zyuiop <zyuiop@zyuiop.net>
2017-11-23 00:56:38 +0100

    New not found message in french

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   template: `<div class="panel panel-danger">
-    <div class="panel-heading">Page not found</div>
+    <div class="panel-heading">Page introuvable</div>
     <div class="panel-body">
-      <p>The requested page cannot be found!</p>
+      <p>La page recherchée n'a pas été trouvée !</p>
     </div>
   </div>`
 })
